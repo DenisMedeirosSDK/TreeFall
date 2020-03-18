be2a8040e53ca86063e9857b83ca5fa4e88c1fbd
Denis Medeiros <48757658+DenisMedeirosKollosso@users.noreply.github.com>
2020-03-18 14:41:45 -0300

    Connection DB
    Connect DB mongo DB and listen port

---
+++ backend/src/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+
+const app = express();
+
+mongoose.connect('mongodb+srv://treefall:treefall@cluster0-fukiy.mongodb.net/TreeFall?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+    useCreateIndex: true,
+});
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
