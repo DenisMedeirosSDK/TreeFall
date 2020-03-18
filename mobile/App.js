52dc78396dbbd0bd248c88da33cee48424a273fc
Denis Medeiros <48757658+DenisMedeirosKollosso@users.noreply.github.com>
2020-03-18 15:04:37 -0300

    render app

---
+++ mobile/App.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Routes />
+    </NavigationContainer>
+  )
+};
