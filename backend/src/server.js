a3ea33d05b26ba5d973b7fd83d31bec3b3a37c63
DenisMedeirosSDK <contato.denismedeiros@hotmail.com>
2021-09-20 21:15:40 -0300

    fix: remove secret in code

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,21 @@
+require('dotenv/config');
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 
-
 const app = express();
 
-mongoose.connect('mongodb+srv://treefall:treefall@cluster0-fukiy.mongodb.net/TreeFall?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: true,
-  useCreateIndex: true,
-});
+const { user_mongo, password_mongo, collection_name_mongo } = process.env;
+
+mongoose.connect(
+  `mongodb+srv://${user_mongo}:${password_mongo}@cluster0.hr2tt.mongodb.net/${collection_name_mongo}?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+    useCreateIndex: true,
+  }
+);
 
 app.use(express.json());
 app.use(routes);
